be97a0d3b0e92c3e5dd1733ea88c524c284e6196
echo-harbor <randomacc03420@gmail.com>
2026-03-10 21:27:47 +0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,1 +1,40 @@
+local debugger = {
+  ["Loaded"] = false,
+  ["ExitCode"] = -1,
+  ["ExitCause"] = "",
+}
 
+local runService = game:GetService("RunService")
+
+if not runService:IsClient() then
+  warn("[rbx-debug]: source.lua must be run from client! if you wish to run this server side, use source-server.lua instead.")
+  debugger.ExitCode = 3
+  debugger.ExitCause = "source.lua must be run from client! if you wish to run this server side, use source-server.lua instead."
+end
+
+local dependencies = loadstring(game:HttpGet("https://raw.githubusercontent.com/echo-harbor/rbx-debug/refs/heads/main/dependencies.lua"))
+
+local universe = game.GameId
+
+if not table.find(dependencies.SupportedUniverses, universe) then
+  debugger.ExitCode = 2
+  debugger.ExitCause = "universe ".. tostring(universe) .. " is not a supported universe"
+end
+
+if debugger.ExitCode ~= 0 then
+  if debugger.ExitCause == "" then
+    warn("[rbx-debug]: debugger has exited with error code ".. tostring(debugger.ExitCode) .. " with no provided reason")
+  else
+    warn("[rbx-debug]: debugger has exited with error code ".. tostring(debugger.ExitCode))
+    warn("[rbx-debug]:", debugger.ExitCause)
+  end
+  return debugger
+end
+
+debugger = {
+  ["Loaded"] = true,
+  ["ExitCode"] = nil,
+  ["ExitCause"] = nil,
+}
+
+print("[rbx-debug]: debugger has successfully loaded into the environment")
