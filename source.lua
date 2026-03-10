0b014f4a8ea24045831d6ddd6aee5cf27e9ed7d7
echo-harbor <randomacc03420@gmail.com>
2026-03-10 21:35:50 +0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,24 +1,30 @@
 local debugger = {
   ["Loaded"] = false,
-  ["ExitCode"] = 1,
+  ["ExitCode"] = -999,
   ["ExitCause"] = "",
 }
 
-local runService = game:GetService("RunService")
+pcall(function()
+  local runService = game:GetService("RunService")
 
-if not runService:IsClient() then
-  warn("[rbx-debug]: source.lua must be run from client! if you wish to run this server side, use source-server.lua instead.")
-  debugger.ExitCode = 3
-  debugger.ExitCause = "source.lua must be run from client! if you wish to run this server side, use source-server.lua instead."
-end
+  if not runService:IsClient() then
+    warn("[rbx-debug]: source.lua must be run from client! if you wish to run this server side, use source-server.lua instead.")
+    debugger.ExitCode = 3
+    debugger.ExitCause = "source.lua must be run from client! if you wish to run this server side, use source-server.lua instead."
+  end
 
-local dependencies = loadstring(game:HttpGet("https://raw.githubusercontent.com/echo-harbor/rbx-debug/refs/heads/main/dependencies.lua"))()
+  local dependencies = loadstring(game:HttpGet("https://raw.githubusercontent.com/echo-harbor/rbx-debug/refs/heads/main/dependencies.lua"))()
 
-local universe = game.GameId
+  local universe = game.GameId
 
-if not table.find(dependencies.SupportedUniverses, universe) then
-  debugger.ExitCode = 2
-  debugger.ExitCause = "universe ".. tostring(universe) .. " is not a supported universe"
+  if not table.find(dependencies.SupportedUniverses, universe) then
+    debugger.ExitCode = 2
+    debugger.ExitCause = "universe ".. tostring(universe) .. " is not a supported universe"
+  end
+end)
+
+if debugger.ExitCode == -999 then
+  debugger.ExitCode = 0
 end
 
 if debugger.ExitCode ~= 0 then
